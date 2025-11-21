cf36937524c3278e22b3dcb65d64fe9b89c353d8
Mrk-Calvo <calvo081995@gmail.com>
2025-11-20 22:53:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, render_template_string, request, jsonify, session, redirect, url_for
 from flask_socketio import SocketIO
 import sqlite3
@@ -3692,4 +3695,3 @@ if __name__ == '__main__':
     
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
-
